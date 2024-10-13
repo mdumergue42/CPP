41a77c9e03d77968b80a8d513e0f26aae493e0f8
Maelys <maelysdumergue@gmail.com>
2024-10-13 19:42:47 +0200

    declaration

---
+++ CPP01/ex00/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madumerg <madumerg@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 18:28:01 by madumerg          #+#    #+#             */
+/*   Updated: 2024/10/13 18:46:01 by madumerg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class	Zombie {
+
+	public :
+		void	announce(void);
+
+	private :
+		std::string	name;
+};
+
+#endif
